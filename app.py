077d2a3d7568524db2000491d8e188176a9bafe5
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-06-17 16:37:57 +0100

    translation working now

--- app.py
+++ app.py
@@ -26,24 +26,23 @@ def home():
     
     return render_template('index.html')
 
-
+"""
 @app.route('/word', methods=['GET'])
 def get_word():
     form = Language()
     return render_template('list.html', form=form)
     
-    """
-    url = "https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/father?fields=etymologies&strictMatch=false"
-    r = requests.get(url, headers = {"app_id": app_id, "app_key": app_key})
+    #url = "https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/father?fields=etymologies&strictMatch=false"
+    #r = requests.get(url, headers = {"app_id": app_id, "app_key": app_key})
     
-    d_text = r.json()
+    #d_text = r.json()
     
-    abc = d_text["results"]
-    data = []
-    for i in abc:
-        data.append(i)
+    #abc = d_text["results"]
+    #data = []
+    #for i in abc:
+        #data.append(i)
     
-    return render_template('list.html', data=data)
+    #return render_template('list.html', data=data)
     
     
     #j_form = json.dumps(r.json())
@@ -55,7 +54,7 @@ def get_word():
     
     
     #return d_text
-    """
+
     
 @app.route('/word', methods=['POST'])
 def def_word():
@@ -68,6 +67,65 @@ def def_word():
     
     d_text = r.json()
     return d_text
+"""
+
+
+@app.route('/azure', methods=['GET'])
+def azure_word():
+    form = Language()
+    return render_template('azure.html', form=form)
+
+@app.route('/azure', methods=['POST'])
+def azure_translate():
+    key = os.environ.get('key_var_name')
+    endpoint = "https://api.cognitive.microsofttranslator.com"
+
+    # Add your location, also known as region. The default is global.
+    # This is required if using a Cognitive Services resource.
+    location = os.environ.get('region_var_name')
+    path = '/translate'
+    constructed_url = endpoint + path
+
+    form = Language()
+    to = form.translate.data
+    speech = form.lang.data
+    
+    params = {
+        'api-version': '3.0',
+        'from': 'en',
+        'to': to
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # You can pass more than one object in body.
+    body = [{
+        'text': speech
+    }]
+
+    request = requests.post(constructed_url, params=params, headers=headers, json=body)
+    response = request.json()
+
+    dat = json.dumps(response, sort_keys=True, skipkeys=True, ensure_ascii=False, indent=4, separators=(',', ': '))
+    datt = json.loads(dat)
+    
+    data = []
+    for i in datt:
+        for a in i:
+            dattt = i[a]
+            for o in dattt:
+                data1 = o
+                
+                for r in data1:
+                    data.append(data1[r])
+
+    
+    return render_template('azure.html', result=data, form=form)
 
 
 if __name__ == '__main__':
